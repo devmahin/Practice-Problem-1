bc1fcdb0c331ca902dea9386529714545ec6b4e7
Mahin <111954295+devmahin@users.noreply.github.com>
2023-03-06 15:34:10 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+var marks = 8;
+
+if (marks >= 80 && marks <= 100){
+    console.log("A+");}
+
+else if (marks >= 70 && marks <= 79){
+console.log("A");}
+else if( marks >= 60 && marks <= 69){
+    console.log("A-");}
+else if (marks >= 50 && marks <= 59){
+    console.log("B");}    
+else if( marks >= 40 && marks <= 49){
+        console.log("C");}
+else if (marks >= 33 && marks <= 39){
+        console.log("D");}
+else if (marks >= 0 && marks <= 32){
+            console.log("F");}
+
+
+else {console.log("Worng Writing")}
+
+
+
+var fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits.indexOf("Banana"));
+
+
+
+var fruits = ["Apple", "Banana", "Orange"];
+fruits[1]= "Mango";
+fruits.push("Watermelon");
+console.log(fruits);
+
+
+
+
+
+
+
+        
+        
